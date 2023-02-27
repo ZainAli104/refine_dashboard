6a951bc82dccccc57995177c5adbc2c43f7c136b
Zain Ali <chzainali6363@gmail.com>
2023-02-27 13:56:16 +0500

    add env

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -20,8 +20,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: '794527303566-gvitfaq8dprefl2plvsubf5mhbnulpse.apps.googleusercontent.com',
-          // client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
